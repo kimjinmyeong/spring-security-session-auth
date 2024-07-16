a5878ef1a53f86388575e950d47867178b279da9
kimjinmyeong <kjmdkdlel@gmail.com>
2024-07-16 16:35:47 +0900

    feat: add RedissonConfig

---
+++ src/main/java/io/github/kimjinmyoeng/springsession/config/RedissonConfig.java
@@ -0,0 +1,27 @@
+package io.github.kimjinmyoeng.springsession.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.redisson.spring.data.connection.RedissonConnectionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@Configuration
+@EnableRedisHttpSession
+public class RedissonConfig {
+
+    @Bean
+    public RedissonConnectionFactory redissonConnectionFactory(RedissonClient redisson) {
+        return new RedissonConnectionFactory(redisson);
+    }
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress("redis://127.0.0.1:6379");
+        return Redisson.create(config);
+    }
+}
